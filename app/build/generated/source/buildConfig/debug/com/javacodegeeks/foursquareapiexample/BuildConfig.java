6c3c5ba7003b01b4bfcf03622a52d21fc1c2e145
Marcus Kopp <marcus.kopp86@gmail.com>
2017-08-15 15:46:25 +0200

    refactor

--- app/build/generated/source/buildConfig/debug/com/javacodegeeks/foursquareapiexample/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/javacodegeeks/foursquareapiexample/BuildConfig.java
@@ -5,7 +5,7 @@ package com.javacodegeeks.foursquareapiexample;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.javacodegeeks.foursquareapiexample";
+  public static final String APPLICATION_ID = "com.evenly.marcus_kopp.evenlyhq4square";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
---
+++ app/src/main/java/com/evenly/marcus_kopp/evenlyhq4square/FoursquareVenue.java
@@ -0,0 +1,68 @@
+package com.evenly.marcus_kopp.evenlyhq4square;
+
+public class FoursquareVenue {
+
+    private int distance;
+
+    private String name;
+
+	private String address;
+
+	private String city;
+
+	private String category;
+
+	public FoursquareVenue() {
+		this.name = "";
+		this.city = "";
+		this.address = "";
+        this.distance = 0;
+		this.setCategory("");
+	}
+
+	public String getCity() {
+		if (city.length() > 0) {
+			return city;
+		}
+		return city;
+	}
+
+	public void setCity(String city) {
+		if (city != null) {
+			this.city = city.replaceAll("\\(", "").replaceAll("\\)", "");
+			;
+		}
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getAddress() {
+        return address;
+    }
+
+	public void setName(String name) {
+        this.name = name;
+	}
+
+	public String getName() {
+        return name;
+	}
+
+	public String getCategory() {
+        return category;
+	}
+
+	public void setCategory(String category) {
+        this.category = category;
+	}
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+}
